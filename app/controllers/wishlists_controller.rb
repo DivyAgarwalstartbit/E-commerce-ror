14906c02c99931bc8e001e6d12038ada714846b4
Divy Agarwal <divyagarwal@startbitsolutions.com>
2026-02-11 17:24:10 +0530

    cart commit

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -4,5 +4,10 @@ class WishlistsController < ApplicationController
   def index
     @wishlist = current_user.wishlist || current_user.create_wishlist
     @items = @wishlist.wishlist_items.includes(:product)
+
+    respond_to do |format|
+    format.html { redirect_back fallback_location: root_path }
+    format.js   # create.js.erb
   end
+end
 end
