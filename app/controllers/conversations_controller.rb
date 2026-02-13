662513a84a51585c384c371775a83b2f09025f76
Divy Agarwal <divyagarwal@startbitsolutions.com>
2026-02-13 10:56:04 +0530

    add conversation commit

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -6,4 +6,10 @@ class ConversationsController < ApplicationController
     @conversations = current_user.conversations
   end
 
+  def show
+    @conversation = current_user.conversations.find(params[:id])
+    @messages = @conversation.messages
+  end
+
+  
 end
